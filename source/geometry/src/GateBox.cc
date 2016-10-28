7c446b49f202632fd99c4fb1316e9c85d832728c
Thomas Deschler <thomas@deschler.fr>
2016-10-28 16:27:11 +0200

    Default material changed to G4_Galactic
      + cosmetics

--- source/geometry/src/GateBox.cc
+++ source/geometry/src/GateBox.cc
@@ -19,89 +19,81 @@ See GATE/LICENSE.txt for further details
 
 #include "globals.hh"
 
-//---------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
 // Initialisation
 // Constructor
 //------------------------------------------------------------------------------------------------
 GateBox::GateBox(const G4String& itsName, const G4String& itsMaterialName, //DS comment to avoid warning
-      	      	 G4double itsXLength, G4double itsYLength, G4double itsZLength,
-		 G4bool itsFlagAcceptChildren, G4int depth)
+                 G4double itsXLength, G4double itsYLength, G4double itsZLength,
+                 G4bool itsFlagAcceptChildren, G4int depth)
   : GateVVolume(itsName, itsFlagAcceptChildren, depth),
-    pBoxSolid(0), pBoxLog(0), pBoxPhys(0), pMessenger(0) 
+    pBoxSolid(0), pBoxLog(0), pBoxPhys(0), pMessenger(0)
 {
   SetMaterialName(itsMaterialName);
   mBoxLength[0] = itsXLength;
   mBoxLength[1] = itsYLength;
   mBoxLength[2] = itsZLength;
-     
+
   pMessenger = new GateBoxMessenger(this);
 }
 //------------------------------------------------------------------------------------------------
 
-//---------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
 GateBox::GateBox(const G4String& itsName,
-		 G4bool itsFlagAcceptChildren,
-		 G4int depth)
+                 G4bool itsFlagAcceptChildren,
+                 G4int depth)
   : GateVVolume(itsName, itsFlagAcceptChildren, depth),
-    pBoxSolid(0), pBoxLog(0), pBoxPhys(0), pMessenger(0) 
+    pBoxSolid(0), pBoxLog(0), pBoxPhys(0), pMessenger(0)
 {
-     
   // Set default material name
-  SetMaterialName("Vacuum");
+  SetMaterialName("G4_Galactic"); // G4_Galactic used instead of Vacuum who was never defined anywhere
   mBoxLength[0] = 1.0*cm;
   mBoxLength[1] = 1.0*cm;
   mBoxLength[2] = 1.0*cm;
-  
+
   pMessenger = new GateBoxMessenger(this);
 }
-//---------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------------------------
 //Destructor
 GateBox::~GateBox()
 {
-         
   delete pMessenger;
-     
 }
-//------------------------------------------------------------------------------------------------   
-   
+//------------------------------------------------------------------------------------------------
+
 //------------------------------------------------------------------------------------------------
 G4LogicalVolume* GateBox::ConstructOwnSolidAndLogicalVolume(G4Material* mater, G4bool flagUpdateOnly)
 {
-  if (!flagUpdateOnly || !pBoxSolid){      
+  if (!flagUpdateOnly || !pBoxSolid) {
     // Solid
     pBoxSolid = new G4Box(GetSolidName(), GetBoxXHalfLength(), GetBoxYHalfLength(), GetBoxZHalfLength());
-  
-   
-    // Logical volume 
-    pBoxLog = new G4LogicalVolume(pBoxSolid, mater, GetLogicalVolumeName()); 
-  
+
+    // Logical volume
+    pBoxLog = new G4LogicalVolume(pBoxSolid, mater, GetLogicalVolumeName());
   }
-  else if (pBoxSolid)
-    {
-   
-      pBoxSolid->SetXHalfLength(GetBoxXHalfLength());
-      pBoxSolid->SetYHalfLength(GetBoxYHalfLength());
-      pBoxSolid->SetZHalfLength(GetBoxZHalfLength());    
+  else if (pBoxSolid) {
+    pBoxSolid->SetXHalfLength(GetBoxXHalfLength());
+    pBoxSolid->SetYHalfLength(GetBoxYHalfLength());
+    pBoxSolid->SetZHalfLength(GetBoxZHalfLength());
+  }
 
-    }
-  
   return pBoxLog;
-}  
+}
 //------------------------------------------------------------------------------------------------
 
 
 //------------------------------------------------------------------------------------------------
-// Sets X Box length 
+// Sets X Box length
 void GateBox::SetBoxXLength(G4double lengthXChoice)
 {
   mBoxLength[0] = lengthXChoice;
-} 
+}
 //------------------------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------------------------
-// Sets Y Box length 
+// Sets Y Box length
 void GateBox::SetBoxYLength(G4double lengthYChoice)
 {
   mBoxLength[1] = lengthYChoice;
@@ -109,24 +101,24 @@ void GateBox::SetBoxYLength(G4double lengthYChoice)
 //------------------------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------------------------
-// Sets Z Box length 
+// Sets Z Box length
 void GateBox::SetBoxZLength(G4double lengthZChoice)
-{ 
+{
   mBoxLength[2] = lengthZChoice;
 }
 //------------------------------------------------------------------------------------------------
-    
+
 //------------------------------------------------------------------------------------------------
 void GateBox::DestroyOwnSolidAndLogicalVolume()
 {
-  if (pBoxLog){
-    delete pBoxLog;}
+  if (pBoxLog)
+    delete pBoxLog;
 
   pBoxLog = 0;
-    
-  if (pBoxSolid){
-    delete pBoxSolid;}    
 
-  pBoxSolid = 0;     
-}  
+  if (pBoxSolid)
+    delete pBoxSolid;
+
+  pBoxSolid = 0;
+}
 //------------------------------------------------------------------------------------------------
